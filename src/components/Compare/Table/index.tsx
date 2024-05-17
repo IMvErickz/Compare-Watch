a3a96e1cb415b9e40556df88e2ce87135c83fb82
IMvErickz <erickspy2003@gmail.com>
2024-05-17 19:55:18 -0300

    feat: table to watch details, images container

--- src/components/Compare/Table/index.tsx
+++ src/components/Compare/Table/index.tsx
@@ -6,6 +6,7 @@ import {
     TableHeader,
     TableRow,
 } from "@/components/ui/table"
+import { useParams } from "react-router-dom";
 
 interface Brand {
     id: string;
@@ -42,12 +43,33 @@ export function WatchTable({
     braceletMaterial,
     dialColor,
     movimentType,
+    extras,
+    originCountry,
+    releaseYear,
     Brand }: WatchTableProps) {
+
+    const param = useParams()
+    const { watchId } = param
+
+    function formatPrice(price: number) {
+        const priceFormated = Intl.NumberFormat('PT-br', { style: 'currency', currency: 'BRL' })
+            .format(price)
+
+        return priceFormated
+    }
+
     return (
         <div className="flex flex-col items-center justify-center w-full">
-            <div className="bg-green-oliver-110 w-full h-6 flex items-center justify-center font-l rounded-t-[4.2px]">
-                <h2 className="text-white text-sm">{name}</h2>
-            </div>
+            {watchId ? (
+                <div className="bg-green-oliver-110 w-full h-max flex items-center justify-start font-l rounded-t-[4.2px]">
+                    <h2 className="text-white text-xl pl-4 leading-8">{name}</h2>
+                </div>
+            ) : (
+                <div className="bg-green-oliver-110 w-full h-6 flex items-center justify-center font-l rounded-t-[4.2px]">
+                    <h2 className="text-white text-sm">{name}</h2>
+                </div>
+            )}
+
             <Table className="flex w-full border-b-solid border-gray-compare-700 border-[1px] rounded-b">
                 <TableHeader className="border-none w-full">
                     <TableRow className="flex flex-col items-center border-none pl-4">
@@ -72,6 +94,19 @@ export function WatchTable({
                         <TableHead className="text-gray-compare-900 w-full h-6 border-b-solid border border-b-[1px] border-t-0 border-r-0 border-l-0 border-b-gray-compare-700  flex flex-col items-start justify-center text-start p-0">
                             Preço sugerido
                         </TableHead>
+                        {watchId && (
+                            <>
+                                <TableHead className="text-gray-compare-900 w-full h-6 border-b-solid border border-b-[1px] border-t-0 border-r-0 border-l-0 border-b-gray-compare-700  flex flex-col items-start justify-center text-start p-0">
+                                    Extras
+                                </TableHead>
+                                <TableHead className="text-gray-compare-900 w-full h-6 border-b-solid border border-b-[1px] border-t-0 border-r-0 border-l-0 border-b-gray-compare-700  flex flex-col items-start justify-center text-start p-0">
+                                    Laçamento
+                                </TableHead>
+                                <TableHead className="text-gray-compare-900 w-full h-6 border-b-solid border border-b-[1px] border-t-0 border-r-0 border-l-0 border-b-gray-compare-700  flex flex-col items-start justify-center text-start p-0">
+                                    País de origem
+                                </TableHead>
+                            </>
+                        )}
                     </TableRow>
                 </TableHeader>
                 <TableBody className="w-full">
@@ -95,8 +130,21 @@ export function WatchTable({
                             {movimentType}
                         </TableCell>
                         <TableCell className="text-gray-compare-900 w-full h-6 border-b-solid border-b-[1px] border-t-0 border-r-0 border-l-0 border-gray-compare-700 flex flex-col items-end justify-center text-start p-0">
-                            R$ {price}
+                            {formatPrice(price)}
                         </TableCell>
+                        {watchId && (
+                            <>
+                                <TableCell className="text-gray-compare-900 w-full h-6 border-b-solid border-b-[1px] border-t-0 border-r-0 border-l-0 border-gray-compare-700 flex flex-col items-end justify-center text-start p-0">
+                                    {extras}
+                                </TableCell>
+                                <TableCell className="text-gray-compare-900 w-full h-6 border-b-solid border-b-[1px] border-t-0 border-r-0 border-l-0 border-gray-compare-700 flex flex-col items-end justify-center text-start p-0">
+                                    {releaseYear}
+                                </TableCell>
+                                <TableCell className="text-gray-compare-900 w-full h-6 border-b-solid border-b-[1px] border-t-0 border-r-0 border-l-0 border-gray-compare-700 flex flex-col items-end justify-center text-start p-0">
+                                    {originCountry}
+                                </TableCell>
+                            </>
+                        )}
                     </TableRow>
                 </TableBody>
             </Table>
